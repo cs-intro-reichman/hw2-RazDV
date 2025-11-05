49e0c565e3e2e80339a4c95300de36112a5fb3c3
RazDV <raz.david.2002@gmail.com>
2025-11-05 20:20:44 +0200

    Pie Calculator is working

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,20 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) throws Exception {
+        Double n = 1.0;
+        Double EvenOdd = 1.0;
+        Double totalFruction = 0.0;
+        int numOfTerms = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < numOfTerms; i++) {
+            Double fruction = EvenOdd/n;
+            totalFruction += fruction; 
+            double pie = 4 * totalFruction;
+            n += 2;
+            EvenOdd *= -1;
+            if (i == numOfTerms - 1) {
+                System.out.println("pi according to Java: " + Math.PI);
+                System.out.println("pi, approximated:     " + pie);  
+            }
+        }
+    }
 }
