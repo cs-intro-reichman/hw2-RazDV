9b9495cbca78b8edb0d2146ed289b3dbc6b48e39
RazDV <raz.david.2002@gmail.com>
2025-11-06 11:39:32 +0200

    More efficient

--- CalcPi.java
+++ CalcPi.java
@@ -1,20 +1,23 @@
 public class CalcPi {
     public static void main(String[] args) throws Exception {
-        Double n = 1.0;
-        Double EvenOdd = 1.0;
-        Double totalFruction = 0.0;
+        double n = 1.0;
+        double EvenOdd = 1.0;
+        double totalFruction = 0.0;
         int numOfTerms = Integer.parseInt(args[0]);
 
         for (int i = 0; i < numOfTerms; i++) {
-            Double fruction = EvenOdd/n;
+            double fruction = EvenOdd/n;
             totalFruction += fruction; 
-            double pie = 4 * totalFruction;
             n += 2;
             EvenOdd *= -1;
-            if (i == numOfTerms - 1) {
-                System.out.println("pi according to Java: " + Math.PI);
-                System.out.println("pi, approximated:     " + pie);  
-            }
         }
+		double pie = 4 * totalFruction;
+		
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + pie);  
     }
 }
+// max int value is 2147483647:
+// $ java CalcPi 2147483647
+// pi according to Java: 3.141592653589793
+// pi, approximated:     3.141592654053932
